414388c7ba4f46e2cb400ebf58248be51a072fed
Agus Ibrahim <pymobile123@gmail.com>
2025-04-17 10:05:21 +0700

    add cors

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import edge_tts
 import uvicorn
@@ -6,6 +7,15 @@ import base64
 import os,time
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 class TextToSpeechRequest(BaseModel):
     text: str
